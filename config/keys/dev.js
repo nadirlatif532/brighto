4e39262d6bc9d2b57163699818ddf3eb31d74725
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-05-08 12:32:28 +0000

    initial boilerplate

---
+++ config/keys/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'jwtSecret': 'HITbSAn/%ss7IXnB)l=xeR[9DWBGNg_yW',
+  'email': 'muhammad.hasham.2311@gmail.com',
+  'password':'safdar2311'
+};
+/* If using gmail allow "less secure apps" */
---
+++ routes/routes.admin.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const admin = express.Router();
+
+module.exports = admin;
