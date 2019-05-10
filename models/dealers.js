893ebf16cade9b5086f03548491237786966e180
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-05-10 12:39:37 +0000

    Added name for dealer.

--- models/dealers.js
+++ models/dealers.js
@@ -2,6 +2,20 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Dealer = sequelize.define('Dealer', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        isAlpha: {
+          msg: 'Dealer name should only contain alphabets',
+        },
+        len: {
+          args: [2, 254],
+          msg: 'Please provide an alphabetical last name which is 2-254 characters in length'
+        }
+      }
+    },
     longitude: {
       type: DataTypes.STRING,
     },
