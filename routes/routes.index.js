a791a29730b218f9325e3fd314d33e01604cff4e
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2019-05-19 00:00:06 +0500

    fix bug

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,6 +1,6 @@
 const { authenticate } = require("../middlewares/auth.middleware");
 
-module.exports = app => {
+module.exports = ({ app, upload }) => {
   app.use("/api/v1/u", require("./routes.unauth"));
   app.use("/api/v1/a", authenticate, require("./routes.auth"));
   app.use("/api/v1/admin",authenticate, upload.single('image'), require("./routes.admin")); 
