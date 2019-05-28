9aa5a19ea38c514055ab2709448e4aebac5068e4
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-05-28 20:05:04 +0000

    color trends added three shade Id's.

--- controllers/color_trends.controller.js
+++ controllers/color_trends.controller.js
@@ -1,22 +1,53 @@
 const { ColorTrends, Shades } = require('../models');
-
+const fs = require('fs');
+const keys = require('../config/keys')
 exports.getAll = async (req, res) => {
     try {
-        const result = await ColorTrends.findAll({
-            include: [{
-                model: Shades,
-            }]
+        let result = await ColorTrends.findAll({
+            include: [
+                {
+                    model: Shades,
+                    as: 'shade1'
+                },
+                {
+                    model: Shades,
+                    as: 'shade2'
+                },
+                {
+                    model: Shades,
+                    as: 'shade3'
+                },
+            ]
         });
+        result = JSON.parse(JSON.stringify(result));
+        result.map((item) => {
+            item['shade1']['color'] = {r:item['shade1']['r'],g:item['shade1']['g'],b:item['shade1']['b']};
+            delete item['shade1']['r'];
+            delete item['shade1']['g'];
+            delete item['shade1']['b'];
+
+            item['shade2']['color'] = {r:item['shade2']['r'],g:item['shade2']['g'],b:item['shade2']['b']};
+            delete item['shade2']['r'];
+            delete item['shade2']['g'];
+            delete item['shade2']['b'];
+
+            item['shade3']['color'] = {r:item['shade3']['r'],g:item['shade3']['g'],b:item['shade3']['b']};
+            delete item['shade3']['r'];
+            delete item['shade3']['g'];
+            delete item['shade3']['b'];
+        });
+        console.log(result);
         return res.status(200).json({ success: true, data: result });
     } catch (err) {
+        console.log(err);
         return res.status(500).json({ success: false, errors: err });
     }
 };
 
 exports.create = async (req, res) => {
     try {
-        if(!req.body.ShadeId) {
-            throw "Shade id is missing";
+        if(!req.body.shade1Id || !req.body.shade2Id || !req.body.shade3Id) {
+            throw "Shade id's is missing";
         }
         await ColorTrends.create({ ...req.body, image: req.file.filename });
         return res
