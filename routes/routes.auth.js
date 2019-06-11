33ef5753e699407da02c468aa977ce3d99d1a901
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2019-06-11 20:10:38 +0500

    re route unauth routes to auth

--- routes/routes.auth.js
+++ routes/routes.auth.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const auth = express.Router();
 
+auth.use("/family", require("./routes.family"));
+auth.use("/products", require("./routes.products"));
+auth.use("/product", require("./routes.product"));
+auth.use("/shades", require("./routes.shades"));
+auth.use("/shade", require("./routes.shade"));
+auth.use("/pallet", require("./routes.pallet"));
+auth.use("/color-trends", require("./routes.colorTrend"));
+auth.use("/city", require("./routes.city"));
+auth.use("/country", require("./routes.country"));
+auth.use("/category", require("./routes.category"));
+auth.use("/project-type", require("./routes.projectType"));
+auth.use("/surface", require("./routes.surface"));
+auth.use("/finish-type", require("./routes.finishType"));
+auth.use("/dealer", require("./routes.dealer"));
+auth.use("/favourite", require("./routes.favourite"));
+auth.use("/order", require("./routes.order"));
+
 module.exports = auth;
--- routes/routes.index.js
+++ routes/routes.index.js
@@ -3,5 +3,5 @@ const { authenticate } = require("../middlewares/auth.middleware");
 module.exports = ({ app, upload }) => {
   app.use("/api/v1/u", upload.array(), require("./routes.unauth"));
   app.use("/api/v1/a", authenticate, require("./routes.auth"));
-  app.use("/api/v1/admin", upload.single('image'),  require("./routes.admin")); //upload.single is image
+  app.use("/api/v1/admin", upload.single('image'), authenticate, require("./routes.admin"));
 };
