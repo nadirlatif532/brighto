2f7dd0970f29aa7593e2db7801baf87d044f25ac
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2019-06-11 20:20:31 +0500

    fix: minor bug regarding auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,9 +4,9 @@ const {User} = require('../models');
 
 module.exports.authenticate = async (req, res, next) => {
   const header = req.headers.authorization;
-  const token = header.split(' ')[1];
-
-  if (token) {
+  
+  if (header) {
+    const token = header.split(' ')[1];
     jwt.verify(token, keys.jwtSecret, {algorithms: 'HS256'}, (err, decoded) => {
       if (err) return res.json({success: false, message: 'Authorization token invalid'});
 
