db17e6a6333aac51b6351cf0273b7935c867a2c6
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-06-15 07:04:17 +0000

    feat: added cover image attrib.

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -3,5 +3,5 @@ const { authenticate } = require("../middlewares/auth.middleware");
 module.exports = ({ app, upload }) => {
   app.use("/api/v1/u", upload.array(), require("./routes.unauth"));
   app.use("/api/v1/a", authenticate, upload.array(), require("./routes.auth"));
-  app.use("/api/v1/admin", upload.single('image'), authenticate, require("./routes.admin"));
+  app.use("/api/v1/admin", upload.fields([{ name: 'image', maxCount: 1 }, { name: 'coverImage', maxCount: 1 }]), authenticate, require("./routes.admin"));
 };
